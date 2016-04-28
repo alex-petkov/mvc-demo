2db859b7084057bee48f92a0cccc239f92ca427f
Alex <alex.petkov@gmail.com>
2016-04-28 15:21:06 +0300

    Базови проекти

---
+++ src/EBanking/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using EBanking.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace EBanking.Controllers
+{
+    public class UserController : Controller
+    {
+        //
+        // GET: /User/Create
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        //
+        // POST: /User/Create
+        [HttpPost]
+        public ActionResult Register(RegisterUserModel newUser)
+        {
+            if (!ModelState.IsValid)
+                return View(newUser);
+
+            // TODO: instert record into database
+            // TODO: check for a duplicate username and report error
+
+            FormsAuthentication.SetAuthCookie(newUser.Username, false);
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
